7b0d7e300dc676803cb0d45cf12e03e3390188f5
rftriazislam <riazislam0999@gmail.com>
2021-11-29 12:15:17 +0600

    load off

--- resources/views/frontend/include/footer.blade.php
+++ resources/views/frontend/include/footer.blade.php
@@ -60,10 +60,12 @@
 </footer>
 <div id="back2top" style="bottom: 69px"><i class="icon icon-arrow-up"></i></div>
 <div class="ps-site-overlay"></div>
-<div id="loader-wrapper">
+
+{{-- <div id="loader-wrapper">
     <div class="loader-section section-left"></div>
     <div class="loader-section section-right"></div>
-</div>
+</div> --}}
+
 <div class="ps-search" id="site-search"><a class="ps-btn--close" href="#"></a>
     <div class="ps-search__content">
         <form class="ps-form--primary-search" action="do_action" method="post">
