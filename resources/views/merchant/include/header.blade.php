6b91c5f7db52ba0a92f8ddc70b83be67588f5858
rftriazislam <riazislam0999@gmail.com>
2021-12-01 17:42:56 +0600

    merchant head add

--- resources/views/merchant/include/header.blade.php
+++ resources/views/merchant/include/header.blade.php
@@ -124,12 +124,7 @@
                             alt="">{{ Auth::user()->name }}
                     </a>
                     <div class="dropdown-menu dropdown-usermenu pull-right" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="javascript:;"> Profile</a>
-                        <a class="dropdown-item" href="javascript:;">
-                            <span class="badge bg-red pull-right">50%</span>
-                            <span>Settings</span>
-                        </a>
-                        <a class="dropdown-item" href="javascript:;">Help</a>
+                        <a class="dropdown-item" href="{{ route('myprofile') }}"> Profile</a>
                         <a class="dropdown-item" href="{{ route('logout') }}"><i
                                 class="fa fa-sign-out pull-right"></i> Log
                             Out</a>
